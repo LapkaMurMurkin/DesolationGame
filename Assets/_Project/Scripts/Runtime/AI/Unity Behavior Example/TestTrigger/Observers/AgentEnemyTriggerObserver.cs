310ac4ccb189fe2ea1c868df128a34a164409ec0
VolumeBlez <dchinicalov@gmail.com>
2024-10-29 15:04:39 +0300

    Refactor Deer 0.2

--- Assets/_Project/Scripts/Runtime/AI/Unity Behavior Example/TestTrigger/Observers/AgentEnemyTriggerObserver.cs
+++ Assets/_Project/Scripts/Runtime/AI/Unity Behavior Example/TestTrigger/Observers/AgentEnemyTriggerObserver.cs
@@ -3,23 +3,9 @@ using UnityEngine;
 public class AgentEnemyTriggerObserver : MonoBehaviour
 {
     [SerializeField] private EnemyEnterInTrigger _enemyEnterTheTrigger;
-    [SerializeField] private AgentHit _agentHitEvent;
-    // [SerializeField] private .ExampleBehaviourGraph _r;
-    [SerializeField] private Transform _agent;
-    [SerializeField] private LayerMask _targetLayer;
 
     private void OnTriggerEnter(Collider other)
     {
-        // if(other.gameObject.layer == _targetLayer)
         _enemyEnterTheTrigger.SendEventMessage(other.transform);
-
-        Debug.Log("SDFAS");
     }
-
-    [ContextMenu("Hit")]
-    public void Hit()
-    {
-        _agentHitEvent.SendEventMessage(_agent);
-    }
-
 }
