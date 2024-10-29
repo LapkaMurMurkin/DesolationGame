240d799060a250503eb86d5a31544545069d4a0d
VolumeBlez <dchinicalov@gmail.com>
2024-10-29 18:28:40 +0300

    maybe fix deer

--- Assets/_Project/Scripts/Runtime/Player/PlayerBaseAttackCollider.cs
+++ Assets/_Project/Scripts/Runtime/Player/PlayerBaseAttackCollider.cs
@@ -11,4 +11,12 @@ public class PlayerBaseAttackCollider : MonoBehaviour
         if (dummy is not null)
             onDummyCollision.Invoke(dummy);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.TryGetComponent(out IDamageable damageable))
+        {
+            damageable.ApplyDamage(15);
+        }
+    }
 }
